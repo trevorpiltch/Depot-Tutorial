32e0d665efd413ba8a3eb0a827ae2369fb0417d4
Trevor Piltch <trevor@piltch.com>
2022-07-30 21:58:05 -0400

    Validation!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,7 +3,23 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully create.'}
+        format.json { render :show, status: :created, location: @product }
+      else 
+        puts @product.errors.full_messages
+        format.html { render :new }
+        format.json {render json: @product.errors, status: :unprocessable_entity }
+      end 
+    end 
+  end 
 
   test "should get index" do
     get products_url
@@ -17,7 +33,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +50,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
 
